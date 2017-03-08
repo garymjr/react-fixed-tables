4509de538510830818df7e3856ee32bf94a7a762
Gary Murray <gmurray@stotzeq.com>
2017-03-08 08:17:44 -0700

    Add sort back to DataStore

--- src/index.js
+++ src/index.js
@@ -5,35 +5,69 @@ import { FixedTableCell } from './components/FixedTableCell'
 export class DataStore {
   constructor (data = [], indexMap = null) {
     this._data = data
-    this._indexMap = indexMap
+    if (indexMap) {
+      this._indexMap = indexMap
+    } else {
+      this._indexMap = Array.apply(null, {length: data.length}).map(Number.call, Number)
+    }
   }
 
   getSize () {
-    if (this._indexMap) {
-      return this._indexMap.length
-    } else {
-      return this._data.length
-    }
+    return this._indexMap.length
   }
 
   getObjectAt (index) {
-    if (this._indexMap) {
-      return this._data[this._indexMap[index]]
-    } else {
-      return this._data[index]
-    }
+    return this._data[this._indexMap[index]]
   }
 
   getAll () {
-    if (this._indexMap) {
-      let data = []
-      for (let i = 0; i < this.getSize(); i++) {
-        data.push(this.getObjectAt(i))
-      }
-      return data
-    } else {
-      return this._data
+    if (this._indexMap.length === this._data.length) return this._data
+    let data = []
+    for (let i = 0; i < this.getSize(); i++) {
+      data.push(this.getObjectAt(i))
     }
+    return data
+  }
+
+  sortBy (key, reversed = false) {
+    let sortedIndexMap = this._indexMap.slice()
+
+    sortedIndexMap.sort((indexA, indexB) => {
+      let sortVal
+      if (typeof key === 'object') {
+        for (let i = 0; i < key.length; i++) {
+          sortVal = 0
+          let valueA = isNum ? parseFloat(this._data[indexA][key]) : this._data[indexA][key]
+          let valueB = isNum ? parseFloat(this._data[indexB][key]) : this._data[indexB][key]
+          if (valueA > valueB) {
+            sortVal = 1
+          }
+          if (valueA < valueB) {
+            sortVal = -1
+          }
+          if (i === 0 && (sortVal !== 0 && reversed === true)) {
+            sortVal = sortVal * -1
+          }
+          if (sortVal !== 0) break
+        }
+      } else {
+        sortVal = 0
+        let valueA = isNum ? parseFloat(this._data[indexA][key]) : this._data[indexA][key]
+        let valueB = isNum ? parseFloat(this._data[indexB][key]) : this._data[indexB][key]
+        if (valueA > valueB) {
+          sortVal = 1
+        }
+        if (valueA < valueB) {
+          sortVal = -1
+        }
+        if (sortVal !== 0 && reversed === true) {
+          sortVal = sortVal * -1
+        }
+      }
+      return sortVal
+    })
+
+    return new DataStore(this._data, sortedIndexMap)
   }
 }
 
